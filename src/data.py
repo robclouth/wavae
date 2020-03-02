c76391764c60028eefee80a41a992e0d85565648
Antoine <caillon@ircam.fr>
2020-03-02 15:30:45 +0100

    switch to udls

--- src/data.py
+++ src/data.py
@@ -1,71 +1,32 @@
+from udls import SimpleDataset
 import librosa as li
-import resampy
-import lmdb
-from tqdm import tqdm
-from pathlib import Path
-from os import path
-import pickle
+from . import config
+import numpy as np
 import torch
 
 
-def preprocess(wavlocs, samprate, outdb, n_signal):
-    env = lmdb.open(outdb, map_size=10e9, lock=False)
-
-    wavs = []
-    wavlocs = wavlocs.split(",")
-
-    for wavloc in wavlocs:
-        wavs += [str(elm.absolute()) for elm in Path(wavloc).rglob("*.wav")]
-        wavs += [str(elm.absolute()) for elm in Path(wavloc).rglob("*.aif")]
-
-    wavs = tqdm(wavs)
-
-    with env.begin(write=True) as txn:
-        idx = 0
-        skip = 0
-        for wav in wavs:
-            wavs.set_description(path.basename(wav))
-            try:
-                x, sr = li.load(wav, None)
-            except:
-                skip += 1
-                continue
-            x = resampy.resample(x, sr, samprate)
-
-            N = len(x) // n_signal
-            if N == 0:
-                continue
-            if len(x) % n_signal != 0:
-                x = x[:-(len(x) % n_signal)].reshape([N, n_signal])
-            else:
-                x = x.reshape([N, n_signal])
-
-            for elm in x:
-                txn.put(f"{idx:08d}".encode("utf-8"), pickle.dumps(elm))
-                idx += 1
-        print(f"Skipped {skip} files during preprocess")
-        txn.put("length".encode("utf-8"), pickle.dumps(idx))
+def preprocess(name):
+    x = li.load(name, config.SAMPRATE)[0]
+    border = len(x) % config.N_SIGNAL
+    if len(x) < config.N_SIGNAL:
+        x = np.pad(x, (0, config.N_SIGNAL - len(x)), "constant")
+    elif border:
+        x = x[:-border]
+    return x.reshape(-1, config.N_SIGNAL)
 
 
 class Loader(torch.utils.data.Dataset):
-    def __init__(self, database, cat=1):
+    def __init__(self, cat):
         super().__init__()
-        self.env = lmdb.open(database, lock=False)
-        with self.env.begin(write=False) as txn:
-            self.len = txn.get("length".encode("utf-8"))
-            if self.len is not None:
-                self.len = pickle.loads(self.len)
-        self.cat = cat
+        self.dataset = SimpleDataset(config.LMDB_LOC,
+                                     config.WAV_LOC.split(","), preprocess)
+        self.cat = 1
 
     def __len__(self):
-        return self.len
+        return len(self.dataset)
 
-    def __getitem__(self, i):
-        with self.env.begin(write=False) as txn:
-            x = torch.cat([
-                torch.from_numpy(
-                    pickle.loads(
-                        txn.get(f"{(i+t) % self.len:08d}".encode("utf-8"))))
-                for t in range(self.cat)
-            ], -1)
-        return x.unsqueeze(0)
+    def __getitem__(self, idx):
+        return torch.cat([
+            torch.from_numpy(self.dataset[(idx + i) % self.__len__()]).float()
+            for i in range(self.cat)
+        ], -1)
--- train.py
+++ train.py
@@ -13,11 +13,7 @@ from os import path
 config.parse_args()
 
 # PREPARE DATA
-dataset = Loader(config.LMDB_LOC)
-if dataset.len is None:
-    preprocess(config.WAV_LOC, config.SAMPRATE, config.LMDB_LOC,
-               config.N_SIGNAL)
-dataset = Loader(config.LMDB_LOC, 1 if config.TYPE == "melgan" else 5)
+dataset = Loader(1 if config.TYPE == "melgan" else 5)
 dataloader = torch.utils.data.DataLoader(dataset,
                                          batch_size=config.BATCH,
                                          shuffle=True,
